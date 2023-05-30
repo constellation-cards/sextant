c28b8afe41cb568e40b12c133a5adaffe0d954ea
Bill Garrett <garrett@astralfrontier.org>
2023-05-29 21:23:20 -0700

    feat: Incorporate Handlebars into return values

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
+use handlebars::Handlebars;
+
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate serde_json;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> String {
+    let reg = Handlebars::new();
+    let result = reg.render_template("Hello {{name}}", &json!({"name": "foo"}));
+    match result {
+        Ok(s) => String::from(s),
+        Err(_) => "Error".to_string()
+    }
 }
 
 #[launch]
