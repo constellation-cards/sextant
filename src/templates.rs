c6b571eb5a6585d5af57f2d702ddaf45c781129f
Bill Garrett <garrett@astralfrontier.org>
2023-05-29 23:10:59 -0700

    feat: Get template text from a file

--- src/templates.rs
+++ src/templates.rs
@@ -3,7 +3,7 @@ use handlebars::Handlebars;
 pub fn create_templates() -> Handlebars<'static> {
     let mut reg = Handlebars::new();
 
-    reg.register_template_string("standard", "Good afternoon, {{name}}")
+    reg.register_template_string("standard", include_str!("standard.txt"))
         .expect("Unable to register standard template");
 
     reg
