10e4ab70616ff1e1025e258325c0cd2edcc325cc
Bill Garrett <garrett@astralfrontier.org>
2023-05-30 01:44:06 -0700

    Collect JSON from request

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ extern crate rocket;
 extern crate serde_json;
 
 use handlebars::Handlebars;
-use rocket::State;
+use rocket::{serde::json::Json, State};
+use serde_json::Value;
 
 mod templates;
 
@@ -12,11 +13,16 @@ struct ServiceState<'a> {
     registry: Handlebars<'a>,
 }
 
-#[post("/latex/<template>", format = "json")]
-fn get_latex(service_state: &State<ServiceState>, template: &str) -> String {
+// TODO: send JSON data to Handlebars
+#[post("/latex/<template>", format = "json", data = "<cards>")]
+fn get_latex(service_state: &State<ServiceState>, template: &str, cards: Json<Value>) -> String {
     let result = service_state
         .registry
         .render(template, &json!({"name": "foo"}));
+
+    dbg!(cards);
+    // TODO: return 400 type status on error
+
     match result {
         Ok(s) => String::from(s),
         Err(_) => "Error".to_string(),
