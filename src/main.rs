e2b9430d0610aa6b5d8f7f4ecfe2ad4a8e5bb7d8
Bill Garrett <garrett@astralfrontier.org>
2023-11-05 17:22:56 -0800

    Replace handlebars with upon template engine

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,39 @@
 #[macro_use]
 extern crate rocket;
 
-use handlebars::Handlebars;
 use rocket::State;
 use serde_json::Value;
+use upon::Engine;
 
 mod templates;
 
 struct ServiceState<'a> {
-    registry: Handlebars<'a>,
+    registry: Engine<'a>,
 }
 
 #[post("/latex/<template>", format = "json", data = "<body>")]
 fn get_latex(service_state: &State<ServiceState>, template: &str, body: &str) -> String {
     let cards: Value = serde_json::from_str(body).unwrap();
 
-    let result = service_state.registry.render(template, &cards);
+    let result = service_state
+        .registry
+        .template(template)
+        .render(&cards)
+        .to_string();
 
     // TODO: return 400 type status on error
 
     match result {
         Ok(s) => String::from(s),
-        Err(_) => "Error".to_string(),
+        Err(e) => e.to_string(),
     }
 }
 
 #[launch]
 fn rocket() -> _ {
-    let reg: handlebars::Handlebars = templates::create_templates();
+    let engine = templates::create_templates();
 
     rocket::build()
-        .manage(ServiceState { registry: reg })
+        .manage(ServiceState { registry: engine })
         .mount("/", routes![get_latex])
 }
--- src/templates.rs
+++ src/templates.rs
@@ -1,10 +1,14 @@
-use handlebars::Handlebars;
+use upon::{Engine, Syntax};
 
-pub fn create_templates() -> Handlebars<'static> {
-    let mut reg = Handlebars::new();
+pub fn create_templates() -> Engine<'static> {
+    let syntax = Syntax::builder().expr("<=", "=>").block("<<", ">>").build();
+    let mut engine = Engine::with_syntax(syntax);
 
-    reg.register_template_string("standard", include_str!("standard.txt"))
-        .expect("Unable to register standard template");
+    engine
+        .add_template("standard", include_str!("standard.txt"))
+        .expect("Unable to add standard.txt template");
 
-    reg
+    engine.add_filter("replace", |a: &str, b: &str, c: &str| str::replace(a, b, c));
+
+    engine
 }
