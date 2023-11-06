b739947c7ba2fcb52bf12992bd854b7872261235
Bill Garrett <garrett@astralfrontier.org>
2023-11-06 00:34:42 -0800

    #4 add a filter for card descriptions

--- src/templates.rs
+++ src/templates.rs
@@ -10,5 +10,22 @@ pub fn create_templates() -> Engine<'static> {
 
     engine.add_filter("replace", |a: &str, b: &str, c: &str| str::replace(a, b, c));
 
+    engine.add_filter("description", |desc: &str| {
+        let mut s = String::with_capacity(desc.len());
+        for line in desc.lines() {
+            match line.strip_prefix("//") {
+                Some(line) => {
+                    s.push_str("\\textit{");
+                    s.push_str(line);
+                    s.push('}');
+                }
+                None => s.push_str(line),
+            }
+            s.push('\n');
+            s.push('\n');
+        }
+        s
+    });
+
     engine
 }
